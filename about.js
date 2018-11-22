dc66ddcb1279f992a3dc51b52cf0e58f12a53a9c
Chirality <danieltaox@gmail.com>
2018-11-22 14:13:15 -0600

    Add RAQ section and scrolling between sections

---
+++ about.js
@@ -0,0 +1,10 @@
+//Script that redraws the content on the screen depending on which tab has been clicked
+
+function onClick(name){
+    
+}
+
+
+//Script that fades out faq questions and fades out faq answers
+
+
