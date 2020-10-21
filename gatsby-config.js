79ea7275707a4f97c6ef026ded5028052b303529
Chirality <danieltaox@gmail.com>
2020-10-20 21:46:38 -0400

    steal a bunch of plugins/config from pennlabs.org

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Daniel Tao`,
+    description: `The web zone in the interweb place of Daniel X U Dong Tay-o Tau`,
+    author: `@sigmachirality <danieltaox@gmail.com>`,
+    siteUrl: `https://danxtao.com/`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,8 +14,31 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown`,
+        path: `${__dirname}/src/markdown`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/src/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-reading-time`, `gatsby-remark-prismjs`],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    // TODO: https://github.com/pennlabs/website/blob/403a43b04ab695fc099cd2b86f9b626c7f4914da/gatsby-config.js#L40
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
